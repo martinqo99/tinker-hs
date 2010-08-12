613caf352755a3e0af845968f996ec744b75ca35
Daniel de Kok <me@danieldk.eu>
2010-08-12 14:19:25 +0200

    Add a class for data types which can give markers.
    Sometimes, we need markers for special operations on transducers. Normally,
    such markers are represented by a string of characters. However, since FST
    is generic, the edge labels are not necessarily characters. Offer the
    opportunity to define per-datatype markers.

--- fst/FST/OperatorsT.hs
+++ fst/FST/OperatorsT.hs
@@ -1,7 +1,19 @@
-module FST.OperatorsT ((<$>), (<->>), (<=>>)) where
+module FST.OperatorsT ((<$>), (<->>), (<=>>), Marker(..)) where
 
 import FST.TransducerInterface
 import FST.Operators
+
+-- |
+-- Instances of TMarker can return markers for special use for implemented
+-- operators.
+class Marker t where
+    giveMarker :: Int -> Reg t
+
+-- |
+-- Implementation of markers for 'Char': '<' followed by 'n' percent
+-- signs.
+instance Marker Char where
+    giveMarker n = s '<' |> foldl (|>) eps (take n (repeat (s '%')))
 
 -- |
 -- Replacement: build a transducer that replaces 'upper' by 'lower'.
