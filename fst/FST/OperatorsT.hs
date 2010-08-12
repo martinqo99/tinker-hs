ffe174dad4b0128ae96d2e0f9428eef88af3ec6d
Daniel de Kok <me@danieldk.eu>
2010-08-12 18:26:56 +0200

    Add a function to inverse the upper/lower language of a transducer.

--- fst/FST/OperatorsT.hs
+++ fst/FST/OperatorsT.hs
@@ -16,6 +16,18 @@ instance Marker Char where
     giveMarker n = s '<' |> foldl (|>) eps (take n $ repeat $ s '%')
 
 -- |
+-- Inverse the upper and lower language of a transducer.
+inverse :: Eq a => RReg a -> RReg a
+inverse (Cross r1 r2) = Cross r2 r1
+inverse (Comp r1 r2) = Comp (inverse r1) (inverse r2)
+inverse (ProductR r1 r2) = Comp (inverse r1) (inverse r2)
+inverse (UnionR r1 r2) = Comp (inverse r1) (inverse r2)
+inverse (StarR r) = StarR $ inverse r
+inverse (Identity r) = Identity r
+inverse (Relation s1 s2) = Relation s2 s1
+inverse EmptyR = EmptyR
+
+-- |
 -- Replacement: build a transducer that replaces 'upper' by 'lower'.
 (<->>) :: Eq a => Reg a -> Reg a -> RReg a
 (<->>) upper lower = star (idR (complement $ (<$>) $ upper <-> eps)
