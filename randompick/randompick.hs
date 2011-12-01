c6ce56a449d1e561c2f8913f6d0ce775c400f5ce
Daniël de Kok <me@danieldk.eu>
2011-12-01 17:02:04 +0100

    randompick: rewrite using Data.Enumerator.

--- randompick/randompick.hs
+++ randompick/randompick.hs
@@ -1,57 +1,52 @@
 import Prelude hiding (lines)
 
-import qualified Data.ByteString.Lazy as L
-import Data.ByteString.Internal (c2w)
+import Data.Enumerator (Enumeratee, Enumerator, ($$), ($=), joinI, run_)
+import qualified Data.Enumerator.Text as ET
+import qualified Data.Enumerator.List as EL
 import qualified Data.Set as Set
-import Monad (liftM)
+import qualified Data.Text as T
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.Random
-import qualified IO as IO
-
-
-lines :: L.ByteString -> [L.ByteString]
-lines ps
-    | L.null ps = []
-    | otherwise = case search ps of
-                    Nothing -> [ps]
-                    Just n  -> L.take n ps : lines (L.drop (n+1) ps)
-    where search = L.elemIndex $ c2w '\n'
 
 takeSet :: Ord a => Int -> [a] -> Set.Set a
-takeSet n l = takeSet_ n l Set.empty
-    where takeSet_ n (x:xs) acc
-            | Set.size acc == n = acc
-            | otherwise = takeSet_ n xs (Set.insert x acc)
+takeSet n l = takeSet_ l Set.empty
+    where takeSet_ (x:xs) acc
+            | Set.size acc < n = takeSet_ xs (Set.insert x acc)
+            | otherwise        = acc
+          takeSet_ []     acc  = acc -- XXX - do we want this?
 
-selectLines :: Ord a => Set.Set a -> [(a,b)] -> [b]
-selectLines _ [] = []
-selectLines sLines ((line,v):ys)
-    | Set.member line sLines = v : selectLines sLines ys
-    | otherwise = selectLines sLines ys
-
+usage :: IO ()
 usage = putStrLn "Usage: shuffle N file"
 
+parse :: [a] -> IO (a, a)
 parse [n, file] = return (n, file)
 parse _ = usage >> exitFailure
 
+linesError :: IO ()
 linesError = putStrLn "Number of requested lines larger than file lines!"
 
+linesCheck :: Ord a => a -> a -> IO ()
 linesCheck nLines reqNLines
     | reqNLines > nLines = linesError >> exitFailure
     | otherwise = return ()
 
+enumFileLines :: Enum a => a -> FilePath -> Enumerator (a, T.Text) IO b
+enumFileLines n file =
+  ET.enumFile file $= EL.mapAccum (\acc line -> (succ acc, (acc, line))) n
+
+filterLines :: (Monad m, Ord o) => Set.Set o -> Enumeratee (o, a) (o, a) m b
+filterLines lineNums =
+  EL.filter (\(line, _) -> Set.member line lineNums) 
+
+main :: IO ()
 main = do
   args <- getArgs
   (n, file) <- parse args
   let reqNLines = (read n)::Int
-  h <- IO.openFile file IO.ReadMode
-  fLines <- liftM lines $ L.hGetContents h
-  let nLines = length fLines
+  nLines <- run_ (ET.enumFile file $$ EL.fold (\acc _ -> succ acc) 0)
   linesCheck nLines reqNLines
   g <- getStdGen
   let lineNums = takeSet reqNLines $ randomRs (1, nLines) g
-  h <- IO.openFile file IO.ReadMode
-  fLines <- liftM lines $ L.hGetContents h
-  let sLines = selectLines lineNums . zip [1..nLines] $ fLines
-  mapM_ L.putStrLn sLines
+  run_ (enumFileLines 1 file $$ joinI $ filterLines lineNums $$ joinI $ EL.map snd $$ EL.mapM_ (putStrLn . T.unpack))
+
